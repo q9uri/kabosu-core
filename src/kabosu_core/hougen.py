139586c4b64b8babbe1d4e312b3e1179ae0c33ac
q9uri <9ri.nasu@gmail.com>
2025-09-23 14:53:08 +0900

    keihan logic fit to openjtalk

--- src/kabosu_core/hougen.py
+++ src/kabosu_core/hougen.py
@@ -7,20 +7,24 @@ def convert_to_keihan_acc(
     # https://www.akenotsuki.com/kyookotoba/accent/taihi.html#S2
     # 一泊ずらし
     # ずらせない特殊なアクセント対応表:
-    # tokyo acc0, mora3 == LHH, :keihan HHH
-    # tokyo acc0 , mora3== LLH, :keihan HHH
-    #「雪が・山が・川が」等acc1, mora3 chain_flag = 0, = tokyo LHL, :keihan HLL 
+    # この実装での独自ルール
+    # acc == 0  => acc 1
+    # acc == mora
+    # force chainflag to 0
     features = []
 
     for node_index, njd_feature in enumerate(njd_features):
         mora_size = njd_feature["mora_size"] 
         acc = njd_feature["acc"] 
-        chain_flag = njd_feature["chain_flag"] 
         if acc != 0 and acc != mora_size:
-            if chain_flag == -1:
-                _feature["acc"] = acc + 1
-            elif chain_flag == 0:
-                _feature["acc"] = 1
+            _feature["acc"] = acc + 1
+        elif acc != 0 :
+            _feature["acc"] = 1
+        else:
+            _feature["acc"] = acc
+
+        #force chainflag to 0
+        _feature["chain_flag"] = 0
 
         _feature = {}
         for feature_key in njd_feature.keys():
