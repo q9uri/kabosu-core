83f8ee3347c7cb5a54fe695320c9b2cd083af262
q9uri <9ri.nasu@gmail.com>
2025-10-05 17:37:26 +0900

    move marine

---
+++ src/kabosu_core/language/ja/g2p/marine/criterions/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa: F401
+from kabosu_core.language.ja.g2p.marine.criterions.crossentopyloss import CrossEntropyLoss
+from kabosu_core.language.ja.g2p.marine.criterions.log_likelihood import LogLikelhood
--- src/kabosu_core/marine/criterions/__init__.py
+++
@@ -1,3 +0,0 @@
-# flake8: noqa: F401
-from kabosu_core.marine.criterions.crossentopyloss import CrossEntropyLoss
-from kabosu_core.marine.criterions.log_likelihood import LogLikelhood
