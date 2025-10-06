fadb735981493519553ef652ab12da50fc05d9af
q9uri <9ri.nasu@gmail.com>
2025-10-06 19:20:47 +0900

    remove old code

--- src/kabosu_core/language/ja/nlp/asari/preprocess.py
+++
@@ -1,7 +0,0 @@
-from kabosu_core import vibrato
-
-t = vibrato.Tagger(dictionary="ipa-dic")
-
-
-def tokenize(text: str) -> str:
-    return " ".join([ i[0] for i in t(text)])
--- src/kabosu_core/language/ja/nlp/bunkai/constant.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-METACHAR_SENTENCE_BOUNDARY: str = "\u2502"
-METACHAR_LINE_BREAK: str = "\u2581"
