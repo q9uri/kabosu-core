b733a30d8df24063038346d33fac486ce99bff65
q9uri <9ri.nasu@gmail.com>
2025-10-06 18:51:59 +0900

    add assets

---
+++ src/kabosu_core/assets/__init__.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+ASSETS_DIR = Path(__file__).parent
+
+VIBRATO_DICT_DIR = ASSETS_DIR / "multi/nlp/vibrato"
+UNIDIC_LITE_PATH = VIBRATO_DICT_DIR / "unidic-mecab-2-1-2/system.dic.zst"
+IPADIC_PATH = VIBRATO_DICT_DIR / "ipadic-mecab-2-7-0/system.dic.zst"
+JUMANDIC_PATH = VIBRATO_DICT_DIR / "jumandic-mecab-7-0/system.dic.zst"
+KO_DIC_PATH = VIBRATO_DICT_DIR / "ko-dic-mecab-2.1.1/system.dic.zst"
